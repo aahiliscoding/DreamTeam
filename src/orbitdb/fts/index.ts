21dd36100cfd62385db273d311c8041ae3b7f3b1
aahiliscoding <imaahil786@proton.me>
2022-09-13 11:50:28 +0530

    added config orbitdb

--- src/orbitdb/fts/index.ts
+++ src/orbitdb/fts/index.ts
@@ -4,10 +4,14 @@ import { SearchSqlite } from "./sqlite";
 import { SearchPostgres } from "./postgres";
 
 export { SearchProvider } from "./provider";
-export function createSearchProvider(db: Knex): SearchProvider|null {
+export function createSearchProvider(db: Knex): SearchProvider | null {
   switch (db.client.config.client) {
-    case "sqlite3": return new SearchSqlite(db);
-    case "pg": return new SearchPostgres(db);
+    case "sqlite3":
+      return new SearchSqlite(db);
+    case "pg":
+      return new SearchPostgres(db);
+    case "orbitdb":
+      return new SearchOrbigDB(db);
     default:
       console.log("Full Text Search not available with this database. In the future we will provide a backup.");
   }
